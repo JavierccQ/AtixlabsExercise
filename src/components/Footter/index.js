91ad6712189ed165daacf56b191c5d4940a05aed
Campos <javier.campos@thomsonreuters.com>
2020-06-15 00:41:29 -0300

    Act-02

--- src/components/Footter/index.js
+++ src/components/Footter/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./style.css";
-import Logo from "../../assets/OniblueLogo.PNG"
-
+import Logo from "../../assets/OniblueLogo.PNG";
+import twitterIcon from "../../assets/twitter.svg";
+import facebookIcon from "../../assets/facebook.svg";
+import linkedinIcon from "../../assets/linkedin.svg";
+import youtubeIcon from "../../assets/youtube.svg";
+import spinIcon from "../../assets/circle.PNG";
 
 const Footter = () => {
   return (
@@ -14,31 +18,69 @@ const Footter = () => {
           <a href="#">Support</a>
         </div>
         <div className="col-md-2 col-sm-12 FootterLeft">
-        <h5>Product</h5>
+          <h5>Product</h5>
           <a href="#">Features</a>
           <a href="#">Pricing</a>
           <a href="#">Login</a>
         </div>
         <div className="col-md-2 col-sm-12 FootterLeft">
-        <h5>Resources</h5>
+          <h5>Resources</h5>
           <a href="#">Help</a>
           <a href="#">Blog</a>
           <a href="#">Status</a>
         </div>
         <div className="col-md-2 col-sm-12 FootterLeft">
-        <h5>Performance</h5>
+          <h5>Performance</h5>
           <a href="#">Media</a>
           <a href="#">Reach</a>
           <a href="#">Creative</a>
         </div>
         <div className="col-md-2 col-sm-12 FootterLeft">
-        <h5>Contact Us</h5>
+          <h5>Contact Us</h5>
           <a href="#">+44 (0) 77 2123 9241</a>
-          <a href="#">hello@onibluesocial.com</a>
-          <a href="#">Insertar Iconos</a>
+          <a href="#">hello@onibluesocial.com</a>          
+            <div className="inline">
+            <a href="#">
+              <img
+                className="iconsBottom"
+                src={twitterIcon}
+                alt="twitterIcon"
+              />
+            </a>
+            <a href="#">
+              <img
+                className="iconsBottom"
+                src={facebookIcon}
+                alt="facebookIcon"
+              />
+            </a>
+            <a href="#">
+              <img
+                className="iconsBottom"
+                src={linkedinIcon}
+                alt="linkedinIcon"
+              />
+            </a>
+            <a href="#">
+              <img
+                className="iconsBottom"
+                src={youtubeIcon}
+                alt="youtubeIcon"
+              />
+            </a>
+            <a href="#">
+              <img
+                className="iconsBottom"
+                src={spinIcon}
+                alt="spinIcon"
+              />
+            </a>
+            </div>         
         </div>
       </div>
-      <div className="logostyle"> <img src={Logo} alt="Logo"/></div>
+      <div className="logostyle">       
+        <img src={Logo} alt="Logo" />
+      </div>
       <div className="TmMsg allsectionp"> All right reserved Oniblue™ 2018</div>
     </div>
   );
